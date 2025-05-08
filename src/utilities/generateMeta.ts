56e09cfa00b598045f7e890c35401757df1c94ef
Sassiest01 <nthom.1@outlook.com>
2025-05-08 15:55:26 +1000

    Remove references to "Payload Website Template"

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -26,8 +26,8 @@ export const generateMeta = async (args: {
     const ogImage = getImageURL(doc?.meta?.image);
 
     const title = doc?.meta?.title
-        ? doc?.meta?.title + " | Payload Website Template"
-        : "Payload Website Template";
+        ? doc?.meta?.title
+        : "C&I Expo";
 
     return {
         description: doc?.meta?.description,
